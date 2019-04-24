5f82b5415c3a2212e041ebf5ecd050e3c181defe
Thierry Joux <tjoux@hotmail.com>
2019-04-23 19:41:51 -0700

    Updated Styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -10,26 +10,21 @@ const App = () => {
     <div>
       <CalculatorDisplay text = '0' />
       <ActionButton text = 'clear'/>
-      <NumberButton text = '/' style = 'btn-text' color = 'red' />
-      <NumberButton text = '7' style = 'btn-text' color = 'white'/>
-      <NumberButton text = '8' style = 'btn-text' color = 'white'/>
-      <NumberButton text = '9' style = 'btn-text' color = 'white'/>
-      <NumberButton text = 'X' style = 'btn-text' color = 'red' />
-      <NumberButton text = '4' style = 'btn-text' color = 'white'/>
-      <NumberButton text = '5' style = 'btn-text' color = 'white'/>
-      <NumberButton text = '6' style = 'btn-text' color = 'white'/>
-      <NumberButton text = '-' style = 'btn-text' color = 'red'/>
-      <NumberButton text = '1' style = 'btn-text' color = 'white'/>
-      <NumberButton text = '2' style = 'btn-text' color = 'white'/>
-      <NumberButton text = '3' style = 'btn-text' color = 'white'/>
-      <NumberButton text = '+' style = 'btn-text' color = 'red'/>
+      <NumberButton text = '/' style = 'btn-operation' />
+      <NumberButton text = '7' style = 'btn-number' />
+      <NumberButton text = '8' style = 'btn-number' />
+      <NumberButton text = '9' style = 'btn-number' />
+      <NumberButton text = 'X' style = 'btn-operation'  />
+      <NumberButton text = '4' style = 'btn-number' />
+      <NumberButton text = '5' style = 'btn-number' />
+      <NumberButton text = '6' style = 'btn-number' />
+      <NumberButton text = '-' style = 'btn-operation' />
+      <NumberButton text = '1' style = 'btn-number'/>
+      <NumberButton text = '2' style = 'btn-number' />
+      <NumberButton text = '3' style = 'btn-number' />
+      <NumberButton text = '+' style = 'btn-operation' />
       <ActionButton text = '0'/>
-      <NumberButton text = '=' style = 'btn-text' color = 'red'/>
-
-
-      
-
-
+      <NumberButton text = '=' style = 'btn-operation' />
 
     </div>
   );
